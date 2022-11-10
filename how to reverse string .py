1d061c7979148149686aab871b60930a94aa8be6
shimaaelnagar <96240325+shimaaelnagar@users.noreply.github.com>
2022-11-10 16:37:05 +0200

    first commit

---
+++ how to reverse string .py
@@ -0,0 +1,5 @@
+# user input string
+string_before = input("Enter your String you need revers it : ")
+print("string_before : " + string_before)
+# print it reversed
+print("string_after : " + string_before[::-1])
